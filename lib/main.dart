2b2d439faff9337718205432adc1dfb43c8c1884
Muhlisa_Sharifova <2110162@newuu.uz>
2023-11-20 18:33:37 +0000

    change1

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,187 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'LMS App',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/course_list': (context) => CourseListScreen(),
+        '/course_detail': (context) => CourseDetailScreen(),
+        '/assignments': (context) => AssignmentsScreen(),
+        '/user_profile': (context) => UserProfileScreen(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text('Home Screen'),
+        backgroundColor: Colors.lightGreen,
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'Welcome to the Learning Management System',
+            style: TextStyle(
+              fontStyle: FontStyle.italic,
+              color: Color(0xff2c4114),
+              backgroundColor: Colors.lightGreen,
+            ),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/course_list');
+            },
+            child: Text('Explore Courses'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CourseListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Courses'),
+        backgroundColor: Colors.lightGreen,
+      ),
+      body: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text('Course ${index + 1}'),
+            subtitle: Text('Description of Course ${index + 1}'),
+            onTap: () {
+              Navigator.pushNamed(context, '/course_detail', arguments: {
+                'courseTitle': 'Course ${index + 1}',
+                'courseDescription': 'Description of Course ${index + 1}',
+              });
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class CourseDetailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Map<String, dynamic>? args =
+        ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>?;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(args?['courseTitle'] ?? ''),
+        backgroundColor: Colors.lightGreen,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            args?['courseDescription'] ?? '',
+            style: TextStyle(
+              fontStyle: FontStyle.italic,
+              color: Color(0xff2c4114),
+              backgroundColor: Colors.lightGreen,
+            ),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/assignments', arguments: {
+                'courseTitle': args?['courseTitle'] ?? '',
+              });
+            },
+            child: Text('View Assignments'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AssignmentsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Map<String, dynamic>? args =
+        ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>?;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Assignments - ${args?['courseTitle']}'),
+      ),
+      body: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text('Assignment ${index + 1}'),
+            subtitle: Text('Description of Assignment ${index + 1}'),
+            onTap: () {
+              // Handle assignment selection
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class UserProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('User Profile'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            radius: 50,
+            // Add user profile image here
+          ),
+          SizedBox(height: 20),
+          Text(
+            'User Name',
+            style: TextStyle(
+              fontStyle: FontStyle.italic,
+              color: Color(0xff352020),
+            ),
+          ),
+          Text(
+            'user@email.com',
+            style: TextStyle(
+              fontStyle: FontStyle.italic,
+              color: Color(0xff2c4114),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // Add logout functionality
+            },
+            child: Text('Logout'),
+          ),
+        ],
       ),
     );
   }
